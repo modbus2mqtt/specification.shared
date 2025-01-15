8fc8302a078b6db82033337f424b693267485e43
volkmarnissen <volkmar.nissen@gmail.com>
2025-01-15 12:30:29 +0100

    npm run prettier

--- src/types.ts
+++ src/types.ts
@@ -13,7 +13,7 @@ export enum HttpErrorsEnum {
   ErrInvalidParameter = 422,
   SrvErrInternalServerError = 500,
 }
-export const BUS_TIMEOUT_DEFAULT = 500;
+export const BUS_TIMEOUT_DEFAULT = 500
 export enum ModbusRegisterType {
   IllegalFunctionCode = 0,
   Coils = 1,
@@ -32,68 +32,65 @@ export enum EnumStateClasses {
   none = 0,
   measurement = 1,
   total = 2,
-  total_increasing = 3
+  total_increasing = 3,
 }
 export interface IimportMessages {
-  warnings:string,
-  errors:string
+  warnings: string
+  errors: string
 }
 export interface IselectOption extends Iname {
-  key: number;
+  key: number
 }
 
 export interface Iselect extends Islave_class {
-  optionModbusValues?: number[];
-  options?: IselectOption[];
-  identification?: RegExp;
+  optionModbusValues?: number[]
+  options?: IselectOption[]
+  identification?: RegExp
 }
 
 export interface Islave_class {
-  device_class?: string;
+  device_class?: string
 }
 export interface Inumber extends Islave_class {
-  multiplier?: number;
-  offset?: number;
-  numberFormat?: EnumNumberFormat;
-  decimals?:number;
-  state_class?:EnumStateClasses;
-  uom?: string;
-  step?:number;
-  identification?: IminMax;
+  multiplier?: number
+  offset?: number
+  numberFormat?: EnumNumberFormat
+  decimals?: number
+  state_class?: EnumStateClasses
+  uom?: string
+  step?: number
+  identification?: IminMax
 }
 export interface Itext {
-  stringlength: number;
-  identification?: string;
+  stringlength: number
+  identification?: string
 }
 export interface Ivalue {
-  value: string;
+  value: string
 }
 export interface IFunctionCode {
-  registerType: ModbusRegisterType;
-  name: string;
+  registerType: ModbusRegisterType
+  name: string
 }
 export interface Iconverter {
-  name: Converters;
-  registerTypes: ModbusRegisterType[];
+  name: Converters
+  registerTypes: ModbusRegisterType[]
 }
 
 export interface Icvtparameter {
-  reqPara: string[];
-  optPara: string[];
-  needsOptions: boolean;
+  reqPara: string[]
+  optPara: string[]
+  needsOptions: boolean
 }
 
-export function jsonConverter<K, V>(
-  body: Object,
-  cnv: (a: string) => K,
-): Map<K, V> {
-  let m = new Map<K, V>();
+export function jsonConverter<K, V>(body: Object, cnv: (a: string) => K): Map<K, V> {
+  let m = new Map<K, V>()
   for (var prop in body) {
     if (Object.prototype.hasOwnProperty.call(body, prop)) {
-      m.set(cnv(prop), (body as any)[prop]);
+      m.set(cnv(prop), (body as any)[prop])
     }
   }
-  return m;
+  return m
 }
 
 export enum IdentifiedStates {
@@ -115,132 +112,115 @@ export enum VariableTargetParameters {
   noParam = 0,
 }
 
-export type Converters = "number" | "select" | "text" | "binary" | "value";
-export type ConverterParameter = Inumber | Iselect | Itext | Ivalue;
+export type Converters = 'number' | 'select' | 'text' | 'binary' | 'value'
+export type ConverterParameter = Inumber | Iselect | Itext | Ivalue
 
 export interface Iname {
-  name: string;
+  name: string
 }
 export interface Iid {
-  id: number;
+  id: number
 }
 
 export interface IminMax {
-  min: number;
-  max: number;
+  min: number
+  max: number
 }
 export interface Ientity extends Iid {
-  mqttname?: string;
-  converter: Iconverter;
-  readonly: boolean;
+  mqttname?: string
+  converter: Iconverter
+  readonly: boolean
   variableConfiguration?: {
-    targetParameter: VariableTargetParameters;
-    entityId?: number;
-  };
-  value_template?:string;
-  registerType: ModbusRegisterType;
-  modbusAddress: number;
-  icon?: string;
-  forceUpdate?: boolean;
-  entityCategory?: string;
-  converterParameters?: ConverterParameter;
+    targetParameter: VariableTargetParameters
+    entityId?: number
+  }
+  value_template?: string
+  registerType: ModbusRegisterType
+  modbusAddress: number
+  icon?: string
+  forceUpdate?: boolean
+  entityCategory?: string
+  converterParameters?: ConverterParameter
 }
-export function getParameterType(
-  converter: Iconverter | null | undefined,
-): string | undefined {
+export function getParameterType(converter: Iconverter | null | undefined): string | undefined {
   if (converter)
     switch (converter.name) {
-      case "text":
-        return "Itext";
-      case "number":
-        return "Inumber";
-      case "select":
-        return "Iselect";
-      case "value":
-        return "Ivalue";
-      case "binary":
-        return "";
+      case 'text':
+        return 'Itext'
+      case 'number':
+        return 'Inumber'
+      case 'select':
+        return 'Iselect'
+      case 'value':
+        return 'Ivalue'
+      case 'binary':
+        return ''
       default:
     }
-  return undefined;
+  return undefined
 }
 export function cleanConverterParameters(entity: Ientity) {
-  let o: any = entity.converterParameters;
-  var validKeys: string[] | undefined = undefined;
+  let o: any = entity.converterParameters
+  var validKeys: string[] | undefined = undefined
   switch (getParameterType(entity.converter)) {
-    case "Inumber":
-      validKeys = [
-        "multiplier",
-        "offset",
-        "uom",
-        "device_class",
-        "identification",
-      ];
-      break;
-    case "Itext":
-      validKeys = ["stringlength", "identification"];
-      break;
-    case "Ivalue":
-      validKeys = ["value"];
-      break;
-    case "Iselect":
-      validKeys = ["options"];
-      break;
+    case 'Inumber':
+      validKeys = ['multiplier', 'offset', 'uom', 'device_class', 'identification']
+      break
+    case 'Itext':
+      validKeys = ['stringlength', 'identification']
+      break
+    case 'Ivalue':
+      validKeys = ['value']
+      break
+    case 'Iselect':
+      validKeys = ['options']
+      break
   }
-  if (!validKeys) return;
-  let availableKeys = Object(o);
+  if (!validKeys) return
+  let availableKeys = Object(o)
   for (let k in availableKeys) {
     if (
       validKeys.findIndex((vk) => {
-        return k === vk;
+        return k === vk
       }) < 0
     )
-      delete o[k];
+      delete o[k]
   }
 }
 export function removeModbusData(entity: Ientity) {
-  let o: any = entity;
-  delete o.modbusValue;
-  delete o.mqttValue;
-  delete o.identified;
+  let o: any = entity
+  delete o.modbusValue
+  delete o.mqttValue
+  delete o.identified
 }
 export interface ImodbusEntityAndMessages {
-  ent: ImodbusEntity | undefined;
-  messages: string[];
+  ent: ImodbusEntity | undefined
+  messages: string[]
 }
 export interface ImodbusData {
-  id: number;
-  modbusValue: number[];
-  mqttValue: string | number;
-  identified: IdentifiedStates;
-  modbusError?: string;
+  id: number
+  modbusValue: number[]
+  mqttValue: string | number
+  identified: IdentifiedStates
+  modbusError?: string
 }
 export interface ImodbusEntity extends ImodbusData, Ientity {}
 
 export function instanceOfIentity(object: any): object is Ientity {
-  return (
-    "name" in object &&
-    "converter" in object &&
-    "converterParameters" in object &&
-    "converterOptions" in object
-  );
+  return 'name' in object && 'converter' in object && 'converterParameters' in object && 'converterOptions' in object
 }
 export function instanceOfIModbusEntity(object: any): object is ImodbusEntity {
-  return (
-    instanceOfIentity(object) &&
-    "modbusValue" in object &&
-    "mqttValue" in object
-  );
+  return instanceOfIentity(object) && 'modbusValue' in object && 'mqttValue' in object
 }
 
 declare global {
   interface Array<T> {
-    errormessage: string;
+    errormessage: string
   }
 }
 
-export const SPECIFICATION_VERSION = "0.3";
-export const SPECIFICATION_FILES_VERSION = "0.1";
+export const SPECIFICATION_VERSION = '0.3'
+export const SPECIFICATION_FILES_VERSION = '0.1'
 
 export const enum SpecificationStatus {
   published = 0,
@@ -250,18 +230,18 @@ export const enum SpecificationStatus {
   contributed = 4,
 }
 export const enum SpecificationFileUsage {
-  img = "img",
-  documentation = "doc",
-  icon = "icon",
-  unknown = "",
+  img = 'img',
+  documentation = 'doc',
+  icon = 'icon',
+  unknown = '',
 }
 
 export interface Iidentification {
-  entity: string;
-  min?: number;
-  max?: number;
-  regex?: string;
-  isValid?: boolean;
+  entity: string
+  min?: number
+  max?: number
+  regex?: string
+  isValid?: boolean
 }
 
 export enum FileLocation {
@@ -270,186 +250,152 @@ export enum FileLocation {
 }
 
 export interface IimageAndDocumentUrl {
-  url: string;
-  fileLocation: FileLocation;
-  usage: SpecificationFileUsage;
+  url: string
+  fileLocation: FileLocation
+  usage: SpecificationFileUsage
 }
 export interface ISpecificationText {
-  textId: string;
-  text: string;
+  textId: string
+  text: string
 }
 export interface ISpecificationTexts {
-  lang: string;
-  texts: ISpecificationText[];
+  lang: string
+  texts: ISpecificationText[]
 }
-[];
+;[]
 export interface IUpdatei18nText {
-  key: string;
-  i18n: ISpecificationTexts[];
+  key: string
+  i18n: ISpecificationTexts[]
 }
 
 export interface IbaseSpecification {
-  filename: string;
-  model?: string; // required
-  manufacturer?: string;
-  files: IimageAndDocumentUrl[];
-  status: SpecificationStatus;
-  identification?: Iidentification[];
-  i18n: ISpecificationTexts[];
-  nextEntityId?: number; // required for cloned specs.The entityId should never be reused for a specification regardles of the specification status (public or cloned). Then entity comparision is possible
+  filename: string
+  model?: string // required
+  manufacturer?: string
+  files: IimageAndDocumentUrl[]
+  status: SpecificationStatus
+  identification?: Iidentification[]
+  i18n: ISpecificationTexts[]
+  nextEntityId?: number // required for cloned specs.The entityId should never be reused for a specification regardles of the specification status (public or cloned). Then entity comparision is possible
 }
 
 export interface ImodbusSpecification extends IbaseSpecification {
-  identified: IdentifiedStates;
-  entities: ImodbusEntity[];
-  pullUrl?: string;
+  identified: IdentifiedStates
+  entities: ImodbusEntity[]
+  pullUrl?: string
 }
 export interface Ispecification extends IbaseSpecification {
-  entities: Ientity[];
+  entities: Ientity[]
 }
 
 export interface ImodbusSpecificationAndMessages {
-  spec: ImodbusSpecification | undefined;
-  messages: string[];
+  spec: ImodbusSpecification | undefined
+  messages: string[]
 }
 export function getSpecificationI18nText(
   spec: IbaseSpecification,
   language: string,
   textId: string,
-  noFallbackLanguage: boolean = false,
+  noFallbackLanguage: boolean = false
 ): string | null {
-  if (!spec || !spec.i18n) return null;
-  let texts = spec.i18n.find((i18) => i18.lang === language);
-  let enTexts = spec.i18n.find((i18) => i18.lang === "en");
+  if (!spec || !spec.i18n) return null
+  let texts = spec.i18n.find((i18) => i18.lang === language)
+  let enTexts = spec.i18n.find((i18) => i18.lang === 'en')
   if (texts) {
-    let text = texts.texts.find((tx) => tx.textId === textId);
-    if (text) return text.text;
+    let text = texts.texts.find((tx) => tx.textId === textId)
+    if (text) return text.text
   }
   if (enTexts && !noFallbackLanguage) {
-    let text = enTexts.texts.find((tx) => tx.textId === textId);
-    if (text) return text.text;
+    let text = enTexts.texts.find((tx) => tx.textId === textId)
+    if (text) return text.text
   }
 
-  return null;
+  return null
 }
 export function setSpecificationI18nText(
   spec: IbaseSpecification,
   language: string,
   textId: string,
-  text: string | null | undefined,
+  text: string | null | undefined
 ): void {
-  if (!spec || !spec.i18n) return;
-  let texts = spec.i18n.find((i18) => i18.lang === language);
+  if (!spec || !spec.i18n) return
+  let texts = spec.i18n.find((i18) => i18.lang === language)
   if (!texts) {
-    if (text) spec.i18n.push({ lang: language, texts: [] });
-    else return;
-    texts = spec.i18n[spec.i18n.length - 1];
+    if (text) spec.i18n.push({ lang: language, texts: [] })
+    else return
+    texts = spec.i18n[spec.i18n.length - 1]
   }
-  let textIndex = texts.texts.findIndex((tx) => tx.textId === textId);
+  let textIndex = texts.texts.findIndex((tx) => tx.textId === textId)
 
   if (textIndex >= 0) {
-    if (text) texts.texts[textIndex].text = text;
-    else texts.texts.splice(textIndex, 1);
-  } else if (text) texts.texts.push({ textId: textId, text: text });
+    if (text) texts.texts[textIndex].text = text
+    else texts.texts.splice(textIndex, 1)
+  } else if (text) texts.texts.push({ textId: textId, text: text })
 }
-export function deleteSpecificationI18nText(
-  spec: IbaseSpecification,
-  textId: string,
-): void {
-  if (!spec || !spec.i18n) return;
+export function deleteSpecificationI18nText(spec: IbaseSpecification, textId: string): void {
+  if (!spec || !spec.i18n) return
   spec.i18n.forEach((texts) => {
     if (texts) {
-      let textIndex = texts.texts.findIndex((tx) => tx.textId === textId);
-      if (textIndex >= 0) texts.texts.splice(textIndex, 1);
+      let textIndex = texts.texts.findIndex((tx) => tx.textId === textId)
+      if (textIndex >= 0) texts.texts.splice(textIndex, 1)
     }
-  });
+  })
 }
 
 export function getSpecificationI18nName(
   spec: IbaseSpecification,
   language: string,
-  noFallbackLanguage: boolean = false,
+  noFallbackLanguage: boolean = false
 ): string | null {
-  return getSpecificationI18nText(spec, language, "name", noFallbackLanguage);
+  return getSpecificationI18nText(spec, language, 'name', noFallbackLanguage)
 }
 export function getSpecificationI18nEntityName(
   spec: IbaseSpecification,
   language: string,
   entityId: number,
-  noFallbackLanguage: boolean = false,
+  noFallbackLanguage: boolean = false
 ) {
-  return getSpecificationI18nText(
-    spec,
-    language,
-    "e" + entityId,
-    noFallbackLanguage,
-  );
+  return getSpecificationI18nText(spec, language, 'e' + entityId, noFallbackLanguage)
 }
 export function getSpecificationI18nEntityOptionName(
   spec: IbaseSpecification,
   language: string,
   entityId: number,
   modbusValue: number,
-  noFallbackLanguage: boolean = false,
+  noFallbackLanguage: boolean = false
 ): string | null {
-  return getSpecificationI18nText(
-    spec,
-    language,
-    "e" + entityId + "o." + modbusValue,
-    noFallbackLanguage,
-  );
+  return getSpecificationI18nText(spec, language, 'e' + entityId + 'o.' + modbusValue, noFallbackLanguage)
 }
-export function setSpecificationI18nName(
-  spec: IbaseSpecification,
-  language: string,
-  text: string | null | undefined,
-): void {
-  setSpecificationI18nText(spec, language, "name", text);
+export function setSpecificationI18nName(spec: IbaseSpecification, language: string, text: string | null | undefined): void {
+  setSpecificationI18nText(spec, language, 'name', text)
 }
 export function setSpecificationI18nEntityName(
   spec: IbaseSpecification,
   language: string,
   entityId: number,
-  text: string | null | undefined,
+  text: string | null | undefined
 ): void {
-  setSpecificationI18nText(spec, language, "e" + entityId, text);
+  setSpecificationI18nText(spec, language, 'e' + entityId, text)
 }
 export function setSpecificationI18nEntityOptionName(
   spec: IbaseSpecification,
   language: string,
   entityId: number,
   modbusValue: number,
-  text: string | null | undefined,
+  text: string | null | undefined
 ): void {
-  setSpecificationI18nText(
-    spec,
-    language,
-    "e" + entityId + "o." + modbusValue,
-    text,
-  );
+  setSpecificationI18nText(spec, language, 'e' + entityId + 'o.' + modbusValue, text)
 }
-export function deleteSpecificationI18nEntityOptionName(
-  spec: IbaseSpecification,
-  entityId: number,
-  modbusValue: number,
-): void {
-  deleteSpecificationI18nText(spec, "e" + entityId + "o." + modbusValue);
+export function deleteSpecificationI18nEntityOptionName(spec: IbaseSpecification, entityId: number, modbusValue: number): void {
+  deleteSpecificationI18nText(spec, 'e' + entityId + 'o.' + modbusValue)
 }
 
-export function deleteSpecificationI18nEntityNameAndOptions(
-  spec: IbaseSpecification,
-  entityId: number,
-) {
+export function deleteSpecificationI18nEntityNameAndOptions(spec: IbaseSpecification, entityId: number) {
   spec.i18n.forEach((i18n) => {
-    let textIndex;
-    while (
-      -1 !=
-      (textIndex = i18n.texts.findIndex((tx) =>
-        tx.textId.startsWith("e" + entityId),
-      ))
-    )
-      if (textIndex >= 0) i18n.texts.splice(textIndex, 1);
-  });
+    let textIndex
+    while (-1 != (textIndex = i18n.texts.findIndex((tx) => tx.textId.startsWith('e' + entityId))))
+      if (textIndex >= 0) i18n.texts.splice(textIndex, 1)
+  })
 }
 
 export function getSpecificationI18nEntityOptionId(
@@ -457,39 +403,35 @@ export function getSpecificationI18nEntityOptionId(
   language: string,
   entityId: number,
   mqttValue: string,
-  noFallbackLanguage: boolean = false,
+  noFallbackLanguage: boolean = false
 ): number[] {
-  if (!spec || !spec.i18n) return [0];
-  let texts = spec.i18n.find((i18) => i18.lang === language);
-  let key = "e" + entityId + "o.";
-  let enTexts = spec.i18n.find((i18) => i18.lang === "en");
+  if (!spec || !spec.i18n) return [0]
+  let texts = spec.i18n.find((i18) => i18.lang === language)
+  let key = 'e' + entityId + 'o.'
+  let enTexts = spec.i18n.find((i18) => i18.lang === 'en')
   if (texts) {
-    let text: string | undefined = texts.texts.find(
-      (tx) => tx.text === mqttValue && tx.textId.startsWith(key),
-    )?.textId;
+    let text: string | undefined = texts.texts.find((tx) => tx.text === mqttValue && tx.textId.startsWith(key))?.textId
     if (text) {
-      return [parseInt(text.substring(key.length))];
+      return [parseInt(text.substring(key.length))]
     }
   }
   if (enTexts && !noFallbackLanguage) {
-    let text: string | undefined = enTexts.texts.find(
-      (tx) => tx.text === mqttValue && tx.textId.startsWith(key),
-    )?.textId;
-    if (text) return [parseInt(text.substring(key.length))];
+    let text: string | undefined = enTexts.texts.find((tx) => tx.text === mqttValue && tx.textId.startsWith(key))?.textId
+    if (text) return [parseInt(text.substring(key.length))]
   }
-  return [0];
+  return [0]
 }
 
 export function getCurrentLanguage(_ins: string) {
-  return navigator.language.replace(/\-.*/g, "");
+  return navigator.language.replace(/\-.*/g, '')
 }
 export function getFileNameFromName(name: string): string | undefined {
-  const searchRegExp = /[^a-z^A-Z^\.^0-9+-._]*/g;
-  if (!name) return undefined;
-  let n = name.toLowerCase();
-  return n.replace(searchRegExp, "");
+  const searchRegExp = /[^a-z^A-Z^\.^0-9+-._]*/g
+  if (!name) return undefined
+  let n = name.toLowerCase()
+  return n.replace(searchRegExp, '')
 }
-export const newSpecfilename:string = "_new"
+export const newSpecfilename: string = '_new'
 export const newSpecification: ImodbusSpecification = {
   identified: IdentifiedStates.unknown,
   entities: [],
@@ -497,4 +439,4 @@ export const newSpecification: ImodbusSpecification = {
   files: [],
   filename: newSpecfilename,
   status: SpecificationStatus.new,
-};
+}
